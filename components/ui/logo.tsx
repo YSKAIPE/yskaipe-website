70890720b70de6a24400bf3c4fcd0606f36db029
YSKAIPE <nanonicholas@protonmail.com>
2026-01-19 12:24:13 -0500

    Update logo image source and alt text

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -5,7 +5,7 @@ import logo from "@/public/images/yskaipe-logo-a1.jpg";
 export default function Logo() {
   return (
     <Link href="/" className="inline-flex shrink-0" aria-label="Cruip">
-      <Image src={logo} alt="Cruip Logo" width={32} height={32} />
+      <Image src={logo} alt="YSKAIPE-LOGO" width={305} height={128} />
     </Link>
   );
 }
