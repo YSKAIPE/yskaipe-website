b20510ff704109a559a13141d4e2086ea9ce5e03
YSKAIPE <nanonicholas@protonmail.com>
2026-01-19 12:11:33 -0500

    Change logo image from SVG to JPG

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import logo from "@/public/images/logo.svg";
+import logo from "@/public/images/yskaipe-logo-a1.jpg";
 
 export default function Logo() {
   return (
