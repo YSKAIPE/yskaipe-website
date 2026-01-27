eabd9a8c964fa9edfcfa56ce42243587cc6a5fc5
YSKAIPE <nanonicholas@protonmail.com>
2026-01-27 10:17:05 -0500

    Update cta.tsx

--- components/cta.tsx
+++ components/cta.tsx
@@ -1,30 +1,28 @@
-import Image from "next/image";
-import BlurredShape from "@/public/images/blurred-shape.svg";
+import VideoThumb from "@/public/images/hero-image-02.jpg";
+import ModalVideo from "@/components/modal-video";
 
-export default function Cta() {
+export default function HeroHome() {
   return (
-    <section className="relative overflow-hidden">
-      <div
-        className="pointer-events-none absolute bottom-0 left-1/2 -z-10 -mb-24 ml-20 -translate-x-1/2"
-        aria-hidden="true"
-      >
-        <Image
-          className="max-w-none"
-          src={BlurredShape}
-          width={760}
-          height={668}
-          alt="Blurred shape"
-        />
-      </div>
-      <div className="max-w6xl mx-auto px-4 sm:px-6">
-        <div className="bg-linear-to-r from-transparent via-gray-800/50 py-12 md:py-20">
-          <div className="mx-auto max-w-3xl text-center">
-            <h2
-              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-8 font-nacelle text-3xl font-semibold text-transparent md:text-4xl"
+    <section>
+      <div className="mx-auto max-w-6xl px-4 sm:px-6">
+        {/* Hero content */}
+        <div className="py-12 md:py-20">
+          {/* Section header */}
+          <div className="pb-12 text-center md:pb-20">
+            <h1
+              className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-5 font-nacelle text-4xl font-semibold text-transparent md:text-5xl"
               data-aos="fade-up"
             >
-              Ready to Make your YSKAIPE?
-            </h2>
+              Human Cohesive AI
+            </h1>
+            <div className="mx-auto max-w-3xl">
+              <p
+                className="mb-8 text-xl text-indigo-200/65"
+                data-aos="fade-up"
+                data-aos-delay={200}
+              >
+                YSKAIPE AI makes and manages small real-world project teams using SEED INPUT from an YSKAIPE SUPER USER.
+              </p>
             <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
               <div data-aos="fade-up" data-aos-delay={400}>
                 <a
@@ -48,7 +46,18 @@ export default function Cta() {
                 </a>
               </div>
             </div>
+            </div>
           </div>
+
+          <ModalVideo
+            thumb={VideoThumb}
+            thumbWidth={1104}
+            thumbHeight={576}
+            thumbAlt="Modal video thumbnail"
+            video="videos//video.mp4"
+            videoWidth={1920}
+            videoHeight={1080}
+          />
         </div>
       </div>
     </section>
