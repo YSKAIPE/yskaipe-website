94316e1e22ceca57990998e28573204a94eb60d5
YSKAIPE <nanonicholas@protonmail.com>
2026-02-26 12:27:25 -0500

    Fix duplicate import of Workflows component
    Removed duplicate import of Workflows component.

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -5,9 +5,10 @@ export const metadata = {
 
 import PageIllustration from "@/components/page-illustration";
 import Hero from "@/components/hero-home";
+import Workflows from "@/components/workflows";
 import BuildInPublic from "@/components/build-in-public"; 
 import BetaPods from "@/components/beta-pods"; 
-import Workflows from "@/components/workflows";
+
 import Features from "@/components/features";
 import Testimonials from "@/components/testimonials";
 import Cta from "@/components/cta";
